ebd2c9fbc1ef9440e1001e1c34c24126e78e9608
kintanuki <88674003+kintanuki@users.noreply.github.com>
2021-10-11 11:35:52 -0500

    Add files via upload

---
+++ ejercicio 8.py
@@ -0,0 +1,10 @@
+def area (n1,n2):
+    area=(n1*n2)/2
+    return area
+
+
+n1=float(input("ingresa el perimetro para hallar el área del hexagono:"))
+n2=float(input("ingresa el apotema para hallar el área del hexagono:"))
+
+print("el área de tu hexagono es de:", area(n1,n2))
+
